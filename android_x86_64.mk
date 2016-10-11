258244156d612ca154546349282dd08eb6a93da8
electrikjesus <electrikjesus@gmail.com>
2016-10-11 07:22:17 -0700

    Blissified

--- android_x86_64.mk
+++ android_x86_64.mk
@@ -20,7 +20,7 @@ $(call inherit-product,$(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product,device/generic/common/x86.mk)
 
 # Overrides
-PRODUCT_NAME := cm_android_x86_64
+PRODUCT_NAME := bliss_android_x86_64
 PRODUCT_BRAND := Android-x86
 PRODUCT_DEVICE := android_x86_64
 PRODUCT_MODEL := Generic Android-x86_64
