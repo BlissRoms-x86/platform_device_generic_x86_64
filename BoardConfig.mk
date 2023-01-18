b118534ddce65b124f3c458e536b85beccf3a669
hmtheboy154 <buingoc67@gmail.com>
2023-01-18 14:44:50 +0700

    Support a new libndk_translation repo
    This is for a prebuilt repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,10 @@ ifeq ($(ANDROID_USE_INTEL_HOUDINI),true)
 include vendor/intel/proprietary/houdini/board/native_bridge_arm_on_x86.mk
 endif
 
+ifeq ($(ANDROID_USE_NDK_TRANSLATION),true)
+include vendor/google/proprietary/ndk_translation-prebuilt/board/native_bridge_arm_on_x86.mk
+endif
+
 TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI) $(NATIVE_BRIDGE_ABI_LIST_64_BIT)
 TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI) $(NATIVE_BRIDGE_ABI_LIST_32_BIT)
 TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI) $(TARGET_2ND_CPU_ABI) $(NATIVE_BRIDGE_ABI_LIST_32_BIT) $(NATIVE_BRIDGE_ABI_LIST_64_BIT)
